6e4f52024a08461d623e6332065a10d83546b351
MinguChoi <mingu0629@gmail.com>
2021-06-22 15:45:50 +0900

    add logout

--- frontend/src/components/navigation/Navigation.js
+++ frontend/src/components/navigation/Navigation.js
@@ -1,8 +1,19 @@
+import { faAlignCenter } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 import React from "react"
 import { Link } from "react-router-dom"
 import './Navigation.css'
 
 function Navigation(){
+
+    async function doLogout() {
+        if(window.confirm("로그아웃 하시겠습니까?")){
+            const url = "http://localhost:8080/logout.do";
+            const date = await axios.get(url);
+            sessionStorage.clear();
+            document.location.href = "http://localhost:3000";
+        }
+    }
     function loginCheck(){
         if (sessionStorage.getItem('memberName') == null){
             console.log("값 찍힘");
@@ -26,7 +37,7 @@ function Navigation(){
                     <Link to="/mypage">
                     <span className="member-name">{sessionStorage.getItem('memberName')}</span>
                     </Link>
-                    님 안녕하세요 <button>로그아웃</button>
+                    님 안녕하세요 <button onClick={doLogout}>로그아웃</button>
                 </div>
                 :
                 <div>
